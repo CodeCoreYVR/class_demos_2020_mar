f96ec7c9fe3f3eb74a07c104fb746098dc31a958
Hano <hinsul3113@gmail.com>
2020-03-10 11:39:55 -0700

    Add demo for printing even numbers

--- iterations.js
+++ iterations.js
@@ -1,9 +1,34 @@
-let i = 100;
-while (i >= 0) {
-  console.log(i);
-  i--;
-}
+// let i = 100;
+// while (i >= 0) {
+//   console.log(i);
+//   i--;
+// }
 
-for (let i = 100; i >= 0; i--) {
+// for (let i = 100; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 100; i += 1) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     // use 'continue' keyword in the block of a
+//     // loop to skip the remaining lines of code within
+//     // and go immediately to the next iteration
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+let i = 0;
+while (i <= 100) {
   console.log(i);
+  if (i >= 50) {
+    break; // this will terminate the while loop when i is greater than 50
+  }
+  i += 2;
 }
